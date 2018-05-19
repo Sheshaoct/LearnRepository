ddb3ab84aca1fa61ab11a0e94c391b97ac85fe2e
SHEIK MD <SHEIK MD@SHEIK>
2018-05-19 20:55:33 +0530

    Base String occurrence code

---
+++ src/cases/StringOccur.java
@@ -0,0 +1,27 @@
+package cases;
+import java.util.Scanner;
+
+public class StringOccur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);  
+		System.out.println("Enter a string - ");
+		String inputString=sc.nextLine();
+		System.out.println("Enter a Character - ");
+		char inputChar=sc.next().charAt(0);
+		int counter=0;
+		
+		char[] charArray = inputString.toCharArray();
+		for(char alpha:charArray)
+		{
+			if(alpha==inputChar)
+			{
+				counter++;
+			}
+		}
+		
+		System.out.print(counter);
+	}
+
+}
